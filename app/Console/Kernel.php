6799c443cea27ea5f0c9ff62907b4dd4d8ebfbd3
kimotosenfa <clark194637285@gmail.com>
2021-10-07 14:03:17 +0900

    form2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
         $schedule->command("send:emailsFirst")->everyMinute()->runInBackground();
         $schedule->command("send:emailsSecond")->everyMinute()->runInBackground();
         $schedule->command("send:emailsThird")->everyMinute()->runInBackground();
+		$schedule->command("send:emailsFourth")->everyMinute()->runInBackground();
         $schedule->command("batch:check1")->everyMinute()->runInBackground();
         $schedule->command("batch:check2")->everyMinute()->runInBackground();
         $schedule->command("batch:check3")->everyMinute()->runInBackground();
