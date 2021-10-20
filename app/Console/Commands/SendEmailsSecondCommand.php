a1bd625845441ef7ac38a9d41cee5979301e88b9
kimotosenfa1975 <superdev195128@hotmail.com>
2021-10-20 14:47:36 +0900

    dev

--- app/Console/Commands/SendEmailsSecondCommand.php
+++ app/Console/Commands/SendEmailsSecondCommand.php
@@ -813,7 +813,7 @@ class SendEmailsSecondCommand extends Command
                     }
                 }  
                 catch (\Throwable $e) {
-                    // file_put_contents('html.txt',$e->getMessage());
+                    file_put_contents('html.txt',$e->getMessage());
                     $company->update(['status' => '送信失敗']);
                     $companyContact->update([
                         'is_delivered' => 1
