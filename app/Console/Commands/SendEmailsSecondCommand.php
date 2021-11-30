6c8d206b939463837fa8a65ddaf0eb6cfbc6c20b
kimotosenfa1975 <superdev195128@hotmail.com>
2021-11-30 11:40:27 +0900

    output

--- app/Console/Commands/SendEmailsSecondCommand.php
+++ app/Console/Commands/SendEmailsSecondCommand.php
@@ -55,7 +55,8 @@ class SendEmailsSecondCommand extends Command
         $now = Carbon::now();
         $startTimeCheck = $now->gte($startTimeStamp);
         $endTimeCheck = $now->lte($endTimeStamp);
-
+        $output = new \Symfony\Component\Console\Output\ConsoleOutput();
+        $output->writeln("<info>start</info>");
         if( $startTimeCheck && $endTimeCheck ){
 
             $contacts = Contact::whereHas('reserve_companies')->get();
