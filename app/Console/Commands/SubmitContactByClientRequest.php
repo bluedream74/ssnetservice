74216905082d1a2f97e19303a3d013413c37604b
bluedream74 <bluedream7004@gmail.com>
2022-03-16 09:09:22 +0700

    adjust map key

--- app/Console/Commands/SubmitContactByClientRequest.php
+++ app/Console/Commands/SubmitContactByClientRequest.php
@@ -837,11 +837,13 @@ class SubmitContactByClientRequest extends Command
                             $this->data[$match['name']] = $map['transform'];
                         }
                     }
+                }
+            }
 
-                    if (isset($map['key'])) {
-                        foreach ($map['key'] as $value) {
-                            $this->data[$value] = $map['transform'];
-                        }
+            if (isset($map['key'])) {
+                foreach ($map['key'] as $value) {
+                    if (strpos($this->htmlText, $value) !== false) {
+                        $this->data[$value] = $map['transform'];
                     }
                 }
             }
