4e978b90777210d2e45c313f6ac7c9ad7b7241cf
bluedream74 <bluedream7004@gmail.com>
2022-03-18 10:55:30 +0700

    optimize batch check

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -314,10 +314,11 @@ class DashboardController extends BaseController
     public function batchCheck(){
         $CHECK_CONTACT_FORM = Config::get()->first()->checkContactForm;
         if($CHECK_CONTACT_FORM === 0){
-	    Config::where('id',1)->update(array('registerUrl'=>'1'));
+            Config::where('id',1)->update(array('registerUrl'=>'1'));
             // Artisan::queue('batch:check1', []);
             Config::where('id',1)->update(array('checkContactForm'=>'1'));
-            // Company::where('check_contact_form',1)->update(['check_contact_form'=>0]);
+            Company::whereNotNull('contact_form_url')->update(['check_contact_form'=>1]);
+            Company::whereNull('contact_form_url')->update(['check_contact_form'=>0]);
             return back()->with(['system.message.info' => "一括チェックしています。"]);
         }else {
             Config::where('id',1)->update(array('checkContactForm'=>'0'));
