5305f9a83f2f3e6565f725b3356ac1c2c7cb995b
bluedream74 <bluedream7004@gmail.com>
2022-03-23 17:39:02 +0700

    add edit contact template

---
+++ resources/views/admin/contact_template_edit.blade.php
@@ -0,0 +1,180 @@
+@extends('layouts.admin.app')
+
+@section('content_header_label')
+<h3 class="m-0">テンプレート編集</h3>
+@stop
+@section('content')
+<div class="card">
+  <div class="card-body table-responsive">
+    <div class="modal-body">
+      <div class="row">
+        <label class="col-sm-12">名前<span class="essential">*</span></label>
+        <div class="col-sm-12 form-group row">
+          <div class="col-sm-6">{{ Form::text('surname', $contactTemplate->surname, ['class' => 'form-control', 'id' =>
+            'surname']) }}</div>
+          <div class="col-sm-6">{{ Form::text('lastname', $contactTemplate->lastname, ['class' => 'form-control', 'id'
+            =>
+            'lastname']) }}</div>
+        </div>
+
+        <label class="col-sm-12">フリガナ</label>
+        <div class="col-sm-12 form-group row">
+          <div class="col-sm-6">{{ Form::text('fu_surname', $contactTemplate->fu_surname, ['class' => 'form-control',
+            'id' =>
+            'fu_surname']) }}</div>
+          <div class="col-sm-6">{{ Form::text('fu_lastname', $contactTemplate->fu_lastname, ['class' => 'form-control',
+            'id' =>
+            'fu_lastname']) }}</div>
+        </div>
+
+        <label class="col-sm-12">会社名<span class="essential">*</span></label>
+        <div class="col-sm-12 form-group">
+          {{ Form::text('company', $contactTemplate->company, ['class' => 'form-control','id' => 'company']) }}
+        </div>
+
+        <label class="col-sm-12">メールアドレス<span class="essential">*</span></label>
+        <div class="col-sm-12 form-group">
+          {{ Form::email('email', $contactTemplate->email, ['class' => 'form-control','id' => 'email']) }}
+        </div>
+
+        <label class="col-sm-12">題名<span class="essential">*</span></label>
+        <div class="col-sm-12 form-group">
+          {{ Form::text('title', $contactTemplate->title, ['class' => 'form-control','id' => 'title']) }}
+        </div>
+
+        <label class="col-sm-12">MY URL %myurl%</label>
+        <div class="col-sm-12 form-group">
+          {{ Form::text('myurl', $contactTemplate->myurl, ['class' => 'form-control','id' => 'myurl']) }}
+        </div>
+
+        <label class="col-sm-12">内容<span class="essential">*</span></label>
+        <div class="col-sm-12 form-group">
+          {{ Form::textarea('content', $contactTemplate->content, ['class' => 'form-control', 'rows' => 7, 'id' =>
+          'content']) }}
+        </div>
+
+        <label class="col-sm-12">ホームページURL</label>
+        <div class="col-sm-12 form-group">
+          {{ Form::text('homepageUrl', $contactTemplate->homepageUrl, ['class' => 'form-control','id' => 'homepageUrl'])
+          }}
+        </div>
+
+        <label class="col-sm-12">都道府県</label>
+        <div class="col-sm-8 form-group">
+          {{ Form::select('zone', $prefectures, $contactTemplate->zone, ['class' => 'form-control', 'placeholder' =>
+          'すべて']) }}
+        </div>
+
+        <label class="col-sm-12">郵便番号</label>
+        <div class="col-sm-12 form-group row">
+          <div class="col-sm-6">{{ Form::text('postalCode1', $contactTemplate->postalcode1, ['class' =>
+            'form-control','id' =>
+            'address1']) }}</div>
+          <div class="col-sm-6">{{ Form::text('postalCode2', $contactTemplate->postalcode2, ['class' =>
+            'form-control','id' =>
+            'address1']) }}</div>
+        </div>
+
+        <label class="col-sm-12">住所</label>
+        <div class="col-sm-12 form-group">
+          {{ Form::text('address', $contactTemplate->address, ['class' => 'form-control','id' => 'address']) }}
+        </div>
+
+        <label class="col-sm-12">電話番号</label>
+        <div class="col-sm-12 form-group row">
+          <div class="col-sm-4">{{ Form::number('phoneNumber1', $contactTemplate->phoneNumber1, ['class' =>
+            'form-control','id'
+            => 'phoneNumber1']) }}</div>
+          <div class="col-sm-4">{{ Form::number('phoneNumber2', $contactTemplate->phoneNumber2, ['class' =>
+            'form-control','id'
+            => 'phoneNumber2']) }}</div>
+          <div class="col-sm-4">{{ Form::number('phoneNumber3', $contactTemplate->phoneNumber3, ['class' =>
+            'form-control','id'
+            => 'phoneNumber3']) }}</div>
+        </div>
+
+        <!-- <label class="col-sm-12">添付ファイル</label>
+          <div class="col-sm-12 form-group">
+              <input type="file" name="attachment" class="form-control" />
+          </div> -->
+      </div>
+    </div>
+  </div>
+  <div class="card-footer">
+    <button class="btn btn-sm btn-update btn-primary">
+      Update
+    </button>
+    <button class="btn btn-sm btn-return">
+      Return
+    </button>
+  </div>
+</div>
+
+{{ Form::open(['route' => 'admin.user.stop', 'method' => 'POST', 'id' => 'stopForm']) }}
+{{ Form::hidden('id', '', ['id' => 'stopUserId']) }}
+{{ Form::close() }}
+
+{{ Form::open(['route' => 'admin.user.start', 'method' => 'POST', 'id' => 'startForm']) }}
+{{ Form::hidden('id', '', ['id' => 'startUserId']) }}
+{{ Form::close() }}
+@stop
+
+
+@section('scripts')
+<script>
+  $('.stop').click(function() {
+    var id = $(this).data('id');
+    toastr.fire({
+      html: "このユーザの利用を停止しますか？",
+      showDenyButton: false,
+      showCancelButton: true,
+      showConfirmButton: true,
+      confirmButtonText: "停止",
+      cancelButtonText: "キャンセル",
+      confirmButtonColor: "#dc3545",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      timer: undefined
+    }).then((result) => {
+      if (result.isConfirmed) {
+        $('#stopUserId').val(id);
+        $('#stopForm').submit();
+        $('#showLoading').click();
+      }
+    })
+  })
+  $('.start').click(function() {
+    var id = $(this).data('id');
+    toastr.fire({
+      html: "このユーザの利用を継続しますか？",
+      showDenyButton: false,
+      showCancelButton: true,
+      showConfirmButton: true,
+      confirmButtonText: "継続",
+      cancelButtonText: "キャンセル",
+      confirmButtonColor: "#dc3545",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      timer: undefined
+    }).then((result) => {
+      if (result.isConfirmed) {
+        $('#startUserId').val(id);
+        $('#startForm').submit();
+        $('#showLoading').click();
+      }
+    })
+  })
+</script>
+@stop
+
+<style type="text/css">
+  .card-footer {
+    background-color: #fff !important;
+    border-top: 1px solid #ced4da !important;
+    text-align: right !important;
+  }
+
+  .btn-return {
+    background-color: #c7cfd7 !important;
+  }
+</style>
