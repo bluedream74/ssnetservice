658415d03d42c4b5ea7c90da919d638900b0089a
bluedream74 <bluedream7004@gmail.com>
2022-03-24 11:48:17 +0700

    add template_title

--- database/seeds/ContactTemplateSeeder.php
+++ database/seeds/ContactTemplateSeeder.php
@@ -18,6 +18,33 @@ class ContactTemplateSeeder extends Seeder
                 'id' => 1
             ],
             [
+                'template_title' => "タイトル",
+                'title' => "タイトル",
+                'surname' => "名前",
+                'lastname' => "名前",
+                'fu_surname' => "フリガナ",
+                'fu_lastname' => "フリガナ",
+                'company' => "会社名",
+                'email' => "trashh67@gmail.com",
+                'myurl' => "https://www.google.com/",
+                'content' => "会社名",
+                'homepageUrl' => "https://www.google.com/",
+                'area' => "会社名",
+                'attachment' => null,
+                'postalCode1' => "100",
+                'postalCode2' => "0000",
+                'address' => "会社名",
+                'phoneNumber1' => "09",
+                'phoneNumber2' => "4564",
+                'phoneNumber3' => "4564",
+                'date' => null,
+                'time' => null,
+            ],
+            [
+                'id' => 2
+            ],
+            [
+                'template_title' => "タイトル2",
                 'title' => "タイトル",
                 'surname' => "名前",
                 'lastname' => "名前",
