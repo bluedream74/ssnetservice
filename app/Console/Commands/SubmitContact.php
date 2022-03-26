9aa1dfd68c784d52f4da4876b3e872841692e56d
tung <tung.nd@digidinos.com>
2022-03-26 08:59:29 +0700

    fix syntax error getMapper

--- app/Console/Commands/SubmitContact.php
+++ app/Console/Commands/SubmitContact.php
@@ -516,7 +516,7 @@ class SubmitContact extends Command
             return;
         }
 
-        $mapper = getMapper($companyContact);
+        $mapper = $this->getMapper($companyContact);
 
         foreach ($mapper as $map) {
             // Check if form key contains any string on 'match' array, then use that value
